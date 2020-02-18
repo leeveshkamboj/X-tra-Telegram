c153cd1a6b9c6f5cf462124d46f8e4de88af49e1
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-02-18 21:30:38 +0530

    Update promo.py

--- userbot/plugins/promo.py
+++ userbot/plugins/promo.py
@@ -3,4 +3,4 @@ from userbot.utils import register
 @register(outgoing=True, pattern="^.group$")
 async def join(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Hey there... Get in touch with me and show some love...\nJoin our community:\n\n[Channel](http://t.me/giveaways_24hrs)\n\nChat [group](http://t.me/giveaways24hrsdiscuss)")
+        await e.edit("Hey there... Get in touch with me and show some love...\nJoin our community:\n\n[Channel](http://t.me/giveaways_24hrs)\n\n[Chat group](http://t.me/giveaways24hrsdiscuss)")
